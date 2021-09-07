521d88ee8bee9b3ed3fb12dc0be57f74ac5c9e39
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-09-07 14:41:37 -0300

    :lipstick: style the NewTodo component

--- src/infra/inputs/NewTodo/styles.jsx
+++ src/infra/inputs/NewTodo/styles.jsx
@@ -1,12 +1,36 @@
 import styled from "styled-components/native";
 import { Ionicons } from "@expo/vector-icons";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+	border-radius: 10px;
+	width: 100%;
+	padding: 16px;
+	background: ${({ theme }) => theme.colors.elementsBackground};
+	flex-direction: row;
+	margin-top: 44px;
+`;
 
-export const AddButton = styled.TouchableOpacity``;
+export const AddButton = styled.TouchableOpacity`
+	border-radius: 20px;
+	height: 40px;
+	width: 40px;
+	background: ${({ theme }) => theme.colors.highlightBackground};
+	align-items: center;
+	justify-content: center;
+`;
 
-export const Icon = styled(Ionicons).attrs({
+export const Input = styled.TextInput.attrs(({ theme }) => ({
+	placeholder: "Create a new todo...",
+	placeholderTextColor: theme.colors.grayText,
+}))`
+	flex: 1;
+	font-size: 20px;
+	color: ${({ theme }) => theme.colors.text};
+	margin-left: 16px;
+`;
+
+export const Icon = styled(Ionicons).attrs(({ theme }) => ({
 	name: "checkmark",
-})``;
-
-export const Input = styled.TextInput``;
+	size: 28,
+	color: theme.colors.headerItems,
+}))``;
