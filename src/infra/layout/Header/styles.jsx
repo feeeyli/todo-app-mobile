546c4db6f374eba677927f3610c0aa35338a01c0
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-09-07 12:15:41 -0300

    :lipstick: style the Header component

--- src/infra/layout/Header/styles.jsx
+++ src/infra/layout/Header/styles.jsx
@@ -1,5 +1,16 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+	width: 100%;
+	justify-content: space-between;
+	flex-direction: row;
+`;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+	font-family: sans-serif;
+	font-size: 54px;
+	font-weight: bold;
+	text-transform: uppercase;
+	letter-spacing: 12px;
+	color: ${({ theme }) => theme.colors.elementsBackground};
+`;
