05ac71c662c6b6b27a7ad736d608c66234efdfe3
pedrohsfontoura <pedrohenrisantosfontoura@gmail.com>
2021-09-07 15:12:13 -0300

    :lipstick: style the FilterMethods component

--- src/infra/feedback/FilterMethods/styles.jsx
+++ src/infra/feedback/FilterMethods/styles.jsx
@@ -1,7 +1,20 @@
 import styled from "styled-components/native";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+	border-radius: 10px;
+	width: 100%;
+	margin-top: 20px;
+	padding: 8px 16px;
+	background: ${({ theme }) => theme.colors.elementsBackground};
+	flex-direction: row;
+	justify-content: center;
+`;
 
-export const Filter = styled.TouchableOpacity``;
+export const Filter = styled.TouchableOpacity`
+	padding: 8px 12px;
+`;
 
-export const FilterTitle = styled.Text``;
+export const FilterTitle = styled.Text`
+	color: ${({ theme }) => theme.colors.grayText};
+	font-size: 20px;
+`;
